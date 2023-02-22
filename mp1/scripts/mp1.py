e8e3c701a098a4fb72e8b0cb72529d29631f8ddb
eg11 <eg11@illinois.edu>
2023-02-22 10:54:40 -0600

    added code

--- mp1/scripts/mp1.py
+++ mp1/scripts/mp1.py
@@ -25,7 +25,7 @@ DISENGAGE_MESSAGE_VALUE = 0.0
 
 class BreakForPedestrian():
 
-	def __init__(self):
+    def __init__(self):
         self.rate = rospy.Rate(10)
         
         # Accelerate message init
@@ -36,7 +36,7 @@ class BreakForPedestrian():
         self.accel_cmd.ignore = False
         self.accel_cmd.f64_cmd = DISENGAGE_MESSAGE_VALUE
     
-		# Brake message init
+        # Brake message init
         self.brake_pub = rospy.Publisher('/pacmod/as_rx/brake_cmd', PacmodCmd, queue_size=1)
         self.brake_cmd = PacmodCmd()
         self.brake_cmd.enable = True
@@ -45,10 +45,12 @@ class BreakForPedestrian():
         self.brake_cmd.f64_cmd = DISENGAGE_MESSAGE_VALUE
 
 
-	def run(self):
-		while not rospy.is_shutdown():
+    def run(self):
+        while not rospy.is_shutdown():
+            #todo: get image from stream
+            image = self.get_image_from_stream()
             #todo: determine if human is detected
-            humanDetected = True
+            humanDetected = self.detect_human(image)
         
             if humanDetected:
                 self.brake_cmd.f64_cmd = BREAK_MESSAGE_VALUE
@@ -58,6 +60,7 @@ class BreakForPedestrian():
                 self.accel_pub.publish(self.accel_cmd)
                 
                 print("Braking")
+
             else:
                 self.brake_cmd.f64_cmd = DISENGAGE_MESSAGE_VALUE
                 self.brake_pub.publish(self.brake_cmd)
@@ -70,7 +73,15 @@ class BreakForPedestrian():
             self.rate.sleep()
 
 
+    def get_image_from_stream():
+        return None
+    
+    
+    def detect_human(image):
+        return True
+
+
 if __name__ == '__main__':
-	rospy.init_node('brake_for_pedestrian', anonymous=True)
-	node = BreakForPedestrian()
-	node.run()
+    rospy.init_node('brake_for_pedestrian', anonymous=True)
+    node = BreakForPedestrian()
+    node.run()
